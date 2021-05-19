018985ff433a0d2f894039e609a90a8c1574c15c
Connor Hood <connor_hood@yahoo.com>
2021-05-19 16:01:15 -0500

    started get all songs display method

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class App extends Component {
-    state = {  }
+    state = { 
+        songs: []
+     }
+    
+    componentDidMount(){
+        this.getAllSongs();
+    }
+
+    async getAllSongs(){
+        let response = await axios.get('http://127.0.0.1:8000/music/');
+        this.setState({
+            songs: response.data
+        });
+    }
+    
     render() { 
         return ( 
+            <div>
             <h1>Select your funky fresh tunes here!</h1>
+            <button type="submit" onClick={this.getAllSongs()}>Display Songs</button>
+            </div>
          );
     }
 }
