e01a1888d5d0bc1cbfee11ee623f8614824789eb
Connor Hood <connor_hood@yahoo.com>
2021-05-19 14:37:25 -0500

    Updated text on webpage, installed bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
+import 'bootstrap/dist/css/bootstrap.css';
 
-const jsxElement = <h1>Music Library</h1>;
-console.log(jsxElement);
 
-ReactDOM.render(jsxElement, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
