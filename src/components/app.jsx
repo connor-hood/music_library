3296a35c01d1f8bd42c21717ed9fb8d0e26e29c4
Connor Hood <connor_hood@yahoo.com>
2021-05-24 13:33:02 -0500

    input filter text box, working on comparing values

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,7 +7,8 @@ import './app.css';
 //TODO: Build search bar, get delete button working
 class App extends Component {
     state = { 
-        songs: []
+        songs: [],
+        filtered_songs: ''
      }
     
     componentDidMount(){
@@ -23,21 +24,21 @@ class App extends Component {
         return response
     }
 
-    deleteSong = (index, e) => {
-        const songs = Object.assign([], this.state.songs);
-        songs.splice(index, 1);
-        this.setState({
-            songs: songs
-        })
+    async deleteSong(id){
+        let delResponse = await axios.delete(`http://127.0.0.1:8000/music/${this.state.id}/`);
+        return delResponse
     }
     
     render() { 
         return ( 
             <div>
-            <h1>Select your funky fresh tunes here!</h1>
-            <MusicTable songs={this.state.songs}/>
-            <h4>Create a new song:</h4>
-            <SongForm />
+                <h1>Select your funky fresh tunes here!</h1>
+                <div>
+                    <p>Search: <input type="text" value={this.state.filtered_songs} /></p>
+                </div>
+                <MusicTable songs={this.state.songs}/>
+                <h4>Create a new song:</h4>
+                <SongForm />
             </div>
          );
     }
