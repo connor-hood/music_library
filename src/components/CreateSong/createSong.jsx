9103ada4bb6b1644fb4bbe39ac28434100eaeb84
Connor Hood <connor_hood@yahoo.com>
2021-05-24 08:51:42 -0500

    built form out with input fields getting correct data, will test without prevent default to see if that will add song to library

--- src/components/CreateSong/createSong.jsx
+++ src/components/CreateSong/createSong.jsx
@@ -1,20 +1,35 @@
 import React, {Component} from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 class SongForm extends Component {
-    state = { 
-        title: '',
-        artist: '',
-        album: '',
-        release_date: ''
-     }
+    constructor(props) {
+        super(props);
+        this.state = { 
+            title: '',
+            artist: '',
+            album: '',
+            release_date: ''
+         };
+    }
 
-     componentDidMount(){
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Song Name: ${this.state.title} Artist: ${this.state.artist} Album: ${this.state.album}
+        Release Date: ${this.state.release_date}`)
+    }
+
+    componentDidMount(){
          console.log("Hello")
-         this.createSong();
+        //  this.createSong();
      }
 
-    async createSong() {
+    /* async createSong() {
         let response = await axios.post('http://127.0.0.1:8000/music/', this.state);
         console.log(response)
         this.setState({
@@ -23,27 +38,25 @@ class SongForm extends Component {
             album: response.data,
             release_date: response.data
         });
-        let newTable = await axios.get('http://127.0.0.1:8000/music/')
-        return newTable;
-    }
+    } */
 
     render() { 
         return ( 
             <div>
-                <form>
+                <form onSubmit={(event) => this.handleSubmit(event)}>
                     <label>Song Title</label>
-                    <input type="text" name='title' id='title'></input>
+                    <input type="text" name='title' id='title'onChange={this.handleChange}></input>
                     <br></br>
                     <label>Artist</label>
-                    <input type="text" name='artist' id='artist'></input>
+                    <input type="text" name='artist' id='artist'onChange={this.handleChange}></input>
                     <br></br>
                     <label>Album</label>
-                    <input type="text" name='album' id='album' ></input>
+                    <input type="text" name='album' id='album' onChange={this.handleChange}></input>
                     <br></br>
                     <label>Release Date</label>
-                    <input type="text" name='release_date' id='release_date'></input>
+                    <input type="text" name='release_date' id='release_date'onChange={this.handleChange}></input>
                     <br></br>
-                    <button onClick={this.createSong}>Add to List</button>
+                    <input type='submit'/>
                 </form>
             </div>
          );
